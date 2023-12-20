dbbcca828cbf16ce7262c5e83bbba09b590988f9
Zerhood <Zerhood111@ya.ru>
2023-12-20 09:21:40 +0700

    add methods(equals, hashcode and toString) to the Author and Book classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Author author1 = new Author("Aleksandr", "Griboedov");
-        Author author2 = new Author("Arkadiy and Boris", "Strugatskie");
+        Author author1 = new Author("Александр", "Грибоедов");
+        Author author2 = new Author("Аркадий и Борис", "Стругатские");
 
-        Book book1 = new Book("Gore ot Uma", author1, 1822);
-        Book book2 = new Book("Trudno byt' bogom", author2, 1963);
+        Book book1 = new Book("Горе от Ума", author1, 1822);
+        Book book2 = new Book("Трудно быть богом", author2, 1963);
 
-        book2.setYear(2023);
+//        book2.setYear(2023);
 
         System.out.println(book1);
         System.out.println(book2);
